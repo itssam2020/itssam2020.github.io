10cd194f940777e3d897cece6920d5b780e27be0
CKI <104965941+itssam2020@users.noreply.github.com>
2022-06-08 01:40:37 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+"use strict";
+const content = "Welcome To My Homepage!"
+const text = document.querySelector(".text")
+let index = 0;
+
+ function sleep(delay){ 
+     const start = new Date().getTime(); 
+     while (new Date().getTime() < start + delay); 
+    }
+
+function typing(){
+    text.textContent += content[index++];
+    if(index > content.length){
+        text.textContent = ""
+        index = 0;
+        sleep(3000);
+    }
+}
+setInterval(typing, 200)
